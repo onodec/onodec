1bc8735c1f0c302c5aacd2d923fa21b2528f3b89
senalbulumulle <senal.bulumulle@gmail.com>
2023-02-25 09:23:43 -0800

    Updated Script

--- update-submodules.sh
+++ update-submodules.sh
@@ -5,10 +5,16 @@
 
 
 
-git submodule add https://github.com/onodec/sb-proj-sys-ci-test
-git submodule add https://github.com/onodec/sb-projection-system
-git submodule add https://github.com/onodec/sb-admin-interface
+# git submodule add https://github.com/onodec/sb-proj-sys-ci-test
+# git submodule add https://github.com/onodec/sb-projection-system
+# git submodule add https://github.com/onodec/sb-admin-interface
 
+cd sb-proj-sys-ci-test
+git pull && cd ../
+cd sb-projection-system
+git pull && cd ../
+cd sb-admin-interface
+git pull && cd ../
 
 git add *
 git commit -m "Updated Submodules"
