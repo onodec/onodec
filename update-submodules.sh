66d7916000474e7b37b71d71cd2acd310029ab5f
senalbulumulle <senal.bulumulle@gmail.com>
2023-02-25 09:03:22 -0800

    Updated Submodules

--- update-submodules.sh
+++ update-submodules.sh
@@ -5,10 +5,9 @@
 
 
 
-git submodule add https://github.com/sb-proj-sys-ci-test 
-git submodule add https://github.com/sb-projection-system
-git submodule add https://github.com/sb-admin-interface 
-
+git submodule add https://github.com/onodec/sb-proj-sys-ci-test
+git submodule add https://github.com/onodec/sb-projection-system
+git submodule add https://github.com/onodec/sb-admin-interface
 
 
 git add *
